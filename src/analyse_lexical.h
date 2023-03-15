00848e7088568dcbdedfb9524c057e3ad29ed449
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-03-15 18:24:47 +0100

    hey

---
+++ src/analyse_lexical.h
@@ -0,0 +1,22 @@
+#ifndef PROJET_INTERPRETEUR_ANALYSE_LEXICAL_H
+#define PROJET_INTERPRETEUR_ANALYSE_LEXICAL_H
+
+
+typedef enum
+{
+
+};
+
+
+void advance();
+
+
+Lexeme current_lexeme();
+
+int seq_end();
+
+void stop();
+
+
+
+#endif //PROJET_INTERPRETEUR_ANALYSE_LEXICAL_H
