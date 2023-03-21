d0bbc276b675ae0f3a602f7ca5e319c2a78fa184
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-03-21 17:40:14 +0100

    Update tree.c

--- src/tree.c
+++ src/tree.c
@@ -15,23 +15,46 @@ void free_tree(tree t)
     free_node(t);
 }
 
-// Helper function to recursively print a tree
-static void print_tree_helper(tree t, int depth)
+
+
+/*
+    Helper function to recursively print a tree
+
+    type:   0 -> racine
+            1 -> left
+            2 -> right
+*/
+static void print_tree_helper(tree t, int depth, int type)
 {
     if (t == NULL)
     {
         return;
     }
-    print_tree_helper(t->left, depth + 1);
+    print_tree_helper(t->left, depth + 1, 1);
     for (int i = 0; i < depth; i++)
     {
-        printf("  ");
+        printf("│   ");
+    }
+    switch (type)
+    {
+    case 0:
+        printf("├── ");
+        break;
+    case 1:
+        printf("┌── ");
+        break;
+    case 2:
+        printf("└── ");
+        break;
+    default:
+        exit(1);
     }
     print_node(t);
-    print_tree_helper(t->right, depth + 1);
+    print_tree_helper(t->right, depth + 1, 2);
 }
+
 
 void print_tree(tree t)
 {
-    print_tree_helper(t, 0);
+    print_tree_helper(t, 0, 0);
 }
