265d4c3a03154d5811ddd7d900174cfda15f8ec3
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-04-07 08:08:15 +0200

    remove

--- SolutionCalculette/ast_parcours.h
+++
@@ -1,8 +0,0 @@
-
-void afficherA(Ast expr) ;
-// affiche l'arbre abstrait de l'expression arithmetique expr
-
-int evaluation(Ast expr) ;
-// calcule la valeur de l'expression arithmetique expr
-// FONCTION A COMPLETER !
-
